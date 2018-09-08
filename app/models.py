042a234f53be86fbae5ca8e3ac3b620e8990ff41
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-08 12:07:03 +0300

    password save function added

--- app/models.py
+++ app/models.py
@@ -14,7 +14,16 @@ class User(db.Model):
     pass_key = db.Column(db.String(255))
     pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
 
 
