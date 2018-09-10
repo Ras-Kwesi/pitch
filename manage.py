0d13f2a674a4bc5fbb0dc22cdae120f7194ecc5e
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-10 14:59:54 +0300

    changes

--- manage.py
+++ manage.py
@@ -23,7 +23,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Pitches = Pitches, Comments = Comments, Category = Category)
+    return dict(app = app, db = db, User = User, Pitches = Pitches, Comments = Comments)
 
 
 if __name__ == '__main__':
